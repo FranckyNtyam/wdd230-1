0f87c30cdb8ce53818a933372f41bb6302aa5c56
franckui <francky.ntyam@gmail.com>
2022-09-20 09:05:37 +0100

    modification of script.js file

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,6 @@
 let day = new Date();
 let year = day.getFullYear();
 let time = day.getHours() + ":" + day.getMinutes() + ":" + day.getSeconds();
-let date = day.getDate() + "/" + day.getMonth() + "/" + day.getFullYear();
+let date = (day.getMonth()+1) + "/" + day.getDate() + "/" + day.getFullYear();
 document.getElementById('year').innerHTML = year;
 document.getElementById('current-date').innerHTML = date + " " + time;
